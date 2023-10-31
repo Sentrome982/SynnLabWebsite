bcbb5eceeb7df2ca7033d448268a745e74878093
Evan Hoge <evanhoge123@gmail.com>
2023-10-31 16:08:35 -0400

    Added file uploading. Currently public, so account system needed.

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const multer = require("multer");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
@@ -12,31 +11,6 @@ let io = socketio(server);
 
 app.use(express.static(publicPath));
 
-//Image upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "src/public/images");
-  },
-
-  filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-//const upload = multer({ dest: "../public/image/" });
-
-//Image recieve
-app.get("/upload", (req, res) => {
-  console.log("get");
-  res.sendFile(__dirname + "/public/admin/main.html");
-});
-
-app.post("/upload", upload.single("image"), async (req, res) => {
-  res.send("uploaded");
-});
-
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
@@ -47,5 +21,12 @@ io.on("connection", (socket) => {
   console.log("A user connected"); // optional\
   socket.on("event_created", (eventData) => {
     console.log(eventData);
+  });
+  socket.on("file-upload", (data) => {
+    console.log(data);
+    fs.writeFileSync(
+      path.join("src/public/images/", data.fileName),
+      data.fileData
+    );
   });
 });
